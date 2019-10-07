4d81a25b223cc49c2f5268eb71cf533b5daef859
techguy940 <51826754+techguy940@users.noreply.github.com>
2019-10-07 11:05:23 +0530

    Add files via upload

---
+++ welcom-for-github.py
@@ -0,0 +1,33 @@
+BOT_PREFIX = "Your Bot's Prefix Here"
+BOT_TOKEN = "Your Bot Token Here"
+WELCOME_CHANNEL_ID = Welcome Channel ID Here #Put it without " " Ex - 620498262436347905
+
+import discord
+from discord.ext.commands import Bot
+
+bot = Bot(command_prefix = BOT_PREFIX)
+
+@bot.event
+async def on_ready():
+    print("Logged in as "+bot.user.name)
+
+@bot.event
+async def on_member_join(member):
+    await bot.wait_until_ready()
+    try:
+        channel = bot.get_channel(WELCOME_CHANNEL_ID)
+        try:
+            embed = discord.Embed(colour = discord.Colour.green())
+            embed.set_author(name = member.name,icon_url = member.avatar_url)
+            embed.add_field(name = "Welcome",value = f"**Hey,{member.mention}! Welcome to {member.guild.name}\nI hope you enjoy your stay here!\nThanks for joining**",inline = False)
+            embed.set_thumbnail(url = member.guild.icon_url)
+            await channel.send(embed = embed)
+        except Exception as e:
+            raise e
+    except Exception as e:
+        raise e
+
+
+
+bot.run(BOT_TOKEN)
+

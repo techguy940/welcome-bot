bfeca9e89234089a678e098299a388bdb50a6a17
L0SER <65214509+L0SER8228@users.noreply.github.com>
2020-10-01 08:40:13 +0530

    Update welcome.py

--- welcome.py
+++ welcome.py
@@ -1,6 +1,7 @@
 BOT_PREFIX = "Your Bot's Prefix Here"
 BOT_TOKEN = "Your Bot Token Here"
 WELCOME_CHANNEL_ID = Welcome Channel ID Here #Put it without " " Ex - 620498262436347905
+LEAVE_CHANNEL_ID = Leave Channel ID Here #Put it without " " Ex - 620363449879052624
 
 import discord
 from discord.ext.commands import Bot
@@ -27,7 +28,21 @@ async def on_member_join(member):
     except Exception as e:
         raise e
 
-
+@bot.event
+async def on_member_remove(member):
+    await bot.wait_until_ready()
+    try:
+        channel = bot.get_channel(LEAVE_CHANNEL_ID)
+        try:
+            embed = discord.Embed(colour = discord.Colour.red())
+            embed.set_author(name = member.name,icon_url = member.avatar_url)
+            embed.add_field(name = "Good Bye",value = f"**{member.mention} just left us.**",inline = False)
+            embed.set_thumbnail(url = member.guild.icon_url)
+            await channel.send(embed = embed)
+        except Exception as e:
+            raise e
+    except Exception as e:
+        raise e
 
 bot.run(BOT_TOKEN)
 
